880e255b9da6af01d20a75c8af801d34b6c31a34
HOANG TRUNG TIEN <hoangtrungtien99@gmail.com>
2022-05-12 08:42:14 +0700

    add Login, Signup pages

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import {
   Route,
   Link,
 } from "react-router-dom";
-import About from './components/About';
+import Login from './pages/LogIn';
+import SignUp from './pages/SignUp';
 export default function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<About />} >
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} >
         
           </Route>
 
